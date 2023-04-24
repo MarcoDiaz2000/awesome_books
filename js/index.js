585933e4d6d356fb34c7f6cea2aa716f37edfeff
[faisalakb] <[faisalakb786@gmail.com]>
2023-04-24 17:38:10 +0500

    Updated script file

--- js/index.js
+++ js/index.js
@@ -1,16 +1,29 @@
+function loadData() {
+    let data = document.getElementById("data");
+    if (localStorage.getItem("books")) {
+      data.innerHTML = localStorage.getItem("books");
+    }
+  }
+
 function addBook() {
     let title = document.getElementById("title").value;
     let author = document.getElementById("author").value;
     let data = document.getElementById("data");
-    let book = "Title: " + title + "<br>Author: " + author + "<hr/>";
-    data.innerHTML += "<p>" + book + "</p>";
+    let id = new Date().getTime();
+    let book = `
+      <div id="${id}">
+        <p>Title: ${title}<br>Author: ${author}</p>
+        <button type="button" onclick="removeBook(${id})">Remove</button>
+        <hr/>
+      </div>`;
+    data.innerHTML += book;
+    localStorage.setItem("books", data.innerHTML);
   }
-// save data
-  localStorage.setItem("books", book);
-
-// get data storage
-data.innerHTML = "<p>" + localStorage.getItem("books") + "</p>";
-
-
   
-  
+  function removeBook(id) {
+    let data = document.getElementById("data");
+    let book = document.getElementById(id);
+    data.removeChild(book);
+    let newData = data.innerHTML;
+    localStorage.setItem("books", newData);
+  }
