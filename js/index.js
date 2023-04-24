57b65bfa9c921bf5c96f3a418de806b1932393ad
MarcoDiaz2000 <md.marcodiaz2000@gmail.com>
2023-04-24 05:55:10 -0600

    Add form and addBook function

--- js/index.js
+++ js/index.js
@@ -1,4 +1,15 @@
+function addBook() {
+    let title = document.getElementById("title").value;
+    let author = document.getElementById("author").value;
+    let data = document.getElementById("data");
+    let book = "Title: " + title + "<br>Author: " + author + "<hr/>";
+    data.innerHTML += "<p>" + book + "</p>";
+  }
+// save data
+  localStorage.setItem("books", book);
 
+// get data storage
+data.innerHTML = "<p>" + localStorage.getItem("books") + "</p>";
 
 
   
