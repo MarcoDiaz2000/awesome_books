1d07f1cc0ba2d50436aa0fda8c3ead0f158012d2
[faisalakb] <[faisalakb786@gmail.com]>
2023-04-24 18:03:27 +0500

    Add Linters files

--- js/index.js
+++ js/index.js
@@ -1,29 +1,38 @@
 function loadData() {
-    let data = document.getElementById("data");
-    if (localStorage.getItem("books")) {
-      data.innerHTML = localStorage.getItem("books");
-    }
+  const data = document.getElementById('data');
+  if (localStorage.getItem('books')) {
+    data.innerHTML = localStorage.getItem('books');
   }
+}
 
 function addBook() {
-    let title = document.getElementById("title").value;
-    let author = document.getElementById("author").value;
-    let data = document.getElementById("data");
-    let id = new Date().getTime();
-    let book = `
+  const title = document.getElementById('title').value;
+  const author = document.getElementById('author').value;
+  const data = document.getElementById('data');
+  const id = new Date().getTime();
+  const book = `
       <div id="${id}">
         <p>Title: ${title}<br>Author: ${author}</p>
         <button type="button" onclick="removeBook(${id})">Remove</button>
         <hr/>
       </div>`;
-    data.innerHTML += book;
-    localStorage.setItem("books", data.innerHTML);
-  }
-  
-  function removeBook(id) {
-    let data = document.getElementById("data");
-    let book = document.getElementById(id);
-    data.removeChild(book);
-    let newData = data.innerHTML;
-    localStorage.setItem("books", newData);
-  }
+  data.innerHTML += book;
+  localStorage.setItem('books', data.innerHTML);
+}
+
+function removeBook(id) {
+  const data = document.getElementById('data');
+  const book = document.getElementById(id);
+  data.removeChild(book);
+  const newData = data.innerHTML;
+  localStorage.setItem('books', newData);
+}
+
+window.onload = () => {
+  const a = loadData;
+  const b = addBook;
+  const c = removeBook;
+  console.log(a);
+  console.log(b);
+  console.log(c);
+};
