38e64255dd6ee0451601cf1fad7aac042583d934
MarcoDiaz2000 <md.marcodiaz2000@gmail.com>
2023-04-25 00:51:17 -0600

    Fix ESlinte

--- js/index.js
+++ js/index.js
@@ -1,11 +1,22 @@
+function createBookElement(title, author, id) {
+  return `
+    <div id="${id}">
+      <p>Title: ${title}<br>Author: ${author}</p>
+      <button type="button" onclick="removeBook('${id}')">Remove</button>
+      <hr/>
+    </div>`;
+}
+
+// eslint-disable-next-line no-unused-vars
 function loadData() {
   const data = document.getElementById('data');
   const books = JSON.parse(localStorage.getItem('books')) || [];
-  books.forEach(book => {
+  books.forEach((book) => {
     data.innerHTML += createBookElement(book.title, book.author, book.id);
   });
 }
 
+// eslint-disable-next-line no-unused-vars
 function addBook() {
   const title = document.getElementById('title').value;
   const author = document.getElementById('author').value;
@@ -18,21 +29,13 @@ function addBook() {
   localStorage.setItem('books', JSON.stringify(books));
 }
 
-function createBookElement(title, author, id) {
-  return `
-    <div id="${id}">
-      <p>Title: ${title}<br>Author: ${author}</p>
-      <button type="button" onclick="removeBook('${id}')">Remove</button>
-      <hr/>
-    </div>`;
-}
-
+// eslint-disable-next-line no-unused-vars
 function removeBook(id) {
   const data = document.getElementById('data');
   const book = document.getElementById(id);
   data.removeChild(book);
 
   const books = JSON.parse(localStorage.getItem('books')) || [];
-  const updatedBooks = books.filter(book => book.id !== id);
+  const updatedBooks = books.filter((book) => book.id !== id);
   localStorage.setItem('books', JSON.stringify(updatedBooks));
 }
