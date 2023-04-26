ca9b32237ac492ad3006ea759f562118e6a255fd
[faisalakb] <[faisalakb786@gmail.com]>
2023-04-26 20:12:46 +0500

    Added Nav function

--- js/index.js
+++ js/index.js
@@ -1,4 +1,7 @@
 /*  eslint-disable max-classes-per-file, no-unused-vars */
+const lst = document.getElementById('lst');
+const cont = document.getElementById('cont');
+const addBo = document.getElementById('addNew');
 class Book {
   constructor(title, author, id) {
     this.title = title;
@@ -48,9 +51,39 @@ class BookManager {
     const updatedBooks = books.filter((bookData) => bookData.id !== id);
     localStorage.setItem('books', JSON.stringify(updatedBooks));
   }
+  list1(){
+    console.log("i am in list1");
+    lst.classList.add("lstClass");
+  }
+  cnt(){
+    console.log("i am in contactus");
+    document.getElementById("wrapId").classList.remove("wrap");
+    document.getElementById("wrapId").classList.add("nowrap");
+    cont.classList.add("contClass");
+    document.getElementById("cntID").classList.remove("noLstClass");
+
+  }
+  addBook(){
+    console.log("i am in addBook");
+    document.getElementById("wrapId").classList.remove("nowrap");
+    document.getElementById("wrapId").classList.add("wrap");
+    document.getElementById("cntID").classList.add("noLstClass");
+  }
 }
 
 const bookManager = new BookManager();
+
+lst.addEventListener('click', () => {
+  bookManager.list1();
+});
+
+cont.addEventListener('click', () => {
+  bookManager.cnt();
+});
+
+addBo.addEventListener('click', () => {
+  bookManager.addBook();
+});
 
 function removeBookWrapper(id) {
   BookManager.removeBook(id);
