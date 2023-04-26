95860be843e0ec847b74579c93b2f7ca050a1d67
MarcoDiaz2000 <md.marcodiaz2000@gmail.com>
2023-04-26 00:51:52 -0600

    Refactor JavaScript classes

--- js/index.js
+++ js/index.js
@@ -1,42 +1,43 @@
-function createBookElement(title, author, id) {
-  return `
-    <tr id="${id}">
-      <td class="book-info"> "${title}" by ${author}</td>
-      <td class="remove-button">
-        <button type="button" onclick="removeBook('${id}')">Remove</button>
-      </td>
-    </tr>`;
+class BookManager {
+  createBookElement(title, author, id) {
+    return `
+      <tr id="${id}">
+        <td class="book-info"> "${title}" by ${author}</td>
+        <td class="remove-button">
+          <button type="button" onclick="bookManager.removeBook('${id}')">Remove</button>
+        </td>
+      </tr>`;
+  }
+
+  loadData() {
+    const tbody = document.querySelector('#data tbody');
+    const books = JSON.parse(localStorage.getItem('books')) || [];
+    books.forEach((book) => {
+      tbody.innerHTML += this.createBookElement(book.title, book.author, book.id);
+    });
+  }
+
+  addBook() {
+    const title = document.getElementById('title').value;
+    const author = document.getElementById('author').value;
+    const id = new Date().getTime().toString();
+    const tbody = document.querySelector('#data tbody');
+    tbody.innerHTML += this.createBookElement(title, author, id);
+
+    const books = JSON.parse(localStorage.getItem('books')) || [];
+    books.push({ title, author, id });
+    localStorage.setItem('books', JSON.stringify(books));
+  }
+
+  removeBook(id) {
+    const tbody = document.querySelector('#data tbody');
+    const book = document.getElementById(id);
+    tbody.removeChild(book);
+
+    const books = JSON.parse(localStorage.getItem('books')) || [];
+    const updatedBooks = books.filter((book) => book.id !== id);
+    localStorage.setItem('books', JSON.stringify(updatedBooks));
+  }
 }
 
-// eslint-disable-next-line no-unused-vars
-function loadData() {
-  const tbody = document.querySelector('#data tbody');
-  const books = JSON.parse(localStorage.getItem('books')) || [];
-  books.forEach((book) => {
-    tbody.innerHTML += createBookElement(book.title, book.author, book.id);
-  });
-}
-
-// eslint-disable-next-line no-unused-vars
-function addBook() {
-  const title = document.getElementById('title').value;
-  const author = document.getElementById('author').value;
-  const id = new Date().getTime().toString();
-  const tbody = document.querySelector('#data tbody');
-  tbody.innerHTML += createBookElement(title, author, id);
-
-  const books = JSON.parse(localStorage.getItem('books')) || [];
-  books.push({ title, author, id });
-  localStorage.setItem('books', JSON.stringify(books));
-}
-
-// eslint-disable-next-line no-unused-vars
-function removeBook(id) {
-  const tbody = document.querySelector('#data tbody');
-  const book = document.getElementById(id);
-  tbody.removeChild(book);
-
-  const books = JSON.parse(localStorage.getItem('books')) || [];
-  const updatedBooks = books.filter((book) => book.id !== id);
-  localStorage.setItem('books', JSON.stringify(updatedBooks));
-}
+const bookManager = new BookManager();
